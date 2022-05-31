c2fdb50c38bd25659c2e21c18023c7338789af1c
rganjare <rahul.ganjare@gmail.com>
2022-06-01 01:11:51 +0530

    cart groovy script

--- seed-jobs/cart.groovy
+++ seed-jobs/cart.groovy
@@ -1,13 +1,18 @@
 multibranchPipelineJob('CI-pipelines/cart') {
     branchSources {
-        git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/rganjare/DP_cart.git')
+        // git {
+        //     id('123456789') // IMPORTANT: use a constant and unique identifier
+        //     remote('https://github.com/rganjare/DP_cart.git')
+        // }
+        github {
+            id('23232323') // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId('GitHub-cred')
+            repository('rganjare/DP_cart.git')
         }
     }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
+    // orphanedItemStrategy {
+    //     discardOldItems {
+    //         numToKeep(20)
+    //     }
+    // }
 }
